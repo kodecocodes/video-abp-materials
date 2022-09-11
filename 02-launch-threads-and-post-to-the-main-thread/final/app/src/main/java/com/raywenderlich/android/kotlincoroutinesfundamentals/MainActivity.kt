960141cd34fd2d76c3b41dd63a814730c029cf80
aldo <aldo2122@outlook.com>
2022-09-10 23:56:10 -0500

    Updates dependencies, kotlin version and image source

--- 02-launch-threads-and-post-to-the-main-thread/final/app/src/main/java/com/raywenderlich/android/kotlincoroutinesfundamentals/MainActivity.kt
+++ 02-launch-threads-and-post-to-the-main-thread/final/app/src/main/java/com/raywenderlich/android/kotlincoroutinesfundamentals/MainActivity.kt
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
 
     Thread(Runnable {
-      val imageUrl = URL("https://wallpaperplay.com/walls/full/1/c/7/38027.jpg")
+      val imageUrl = URL("https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__480.jpg")
       val connection = imageUrl.openConnection() as HttpURLConnection
       connection.doInput = true
       connection.connect()
